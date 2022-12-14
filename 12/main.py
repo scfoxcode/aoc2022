e0de4f6103423c6ddaa76f37bb8ddd589633769f
stephen surtees <stephen@stephens-mbp.home>
2022-12-14 20:23:00 +0000

    Solved part 2 of day 12, but it shouldn't work. Got lucky

--- 12/main.py
+++ 12/main.py
@@ -1,27 +1,52 @@
 import sys
+import time
+
+BIGSCORE = 1000000000
 
 class Node: 
-    def __init__(self, x, y, elevation, isStart, isEnd):
+    def __init__(self, x, y, elevation, isEnd):
         self.x = x
         self.y = y
         self.elevation = elevation 
-        self.isStart = isStart 
         self.isEnd = isEnd 
-        self.smallestScore = 100000000
+        self.noPath = False
+        self.smallestScore = BIGSCORE
         self.connections = []
 
-    def scoreNodes(self, visited=[]):
+    def scoreNodes(self, dest, visited, cap):
+        if len(visited) > cap:
+            return # early out if we already have a shorter path for part2
+        if self.noPath == True:
+            return # early out if we've already established no path to finish from here
         if len(visited) < self.smallestScore:
             self.smallestScore = len(visited) 
+
         else:
             return # early out, no point exploring this branch
 
-        #exists = list(filter(lambda n: n.x == node.x and n.y == node.y))
-        for node in self.connections:
+        # try sorting by manhatten distance to go down the more likely routes first
+        # for part 1 this actually makes it slightly slower, but in the general case I know
+        # it is likely much faster. Setting reverse to True slowed it down by 4*
+        dist = lambda a: abs(a.x - dest.x) + abs(a.y - dest.y)
+        sortedC = sorted(self.connections, key=dist, reverse=False)
+
+        if len(self.connections) <1: # flag so we don't search on other paths
+            if not self.isEnd:
+                self.noPath = True
+                print('HAPPENED')
+                # go backwards through visited and mark them all false if only 1 connection
+                for node in reversed(visited):
+                    if len(node.connections) < 2:
+                        print('BLAH')
+                        node.noPath = True
+                    else:
+                        break
+
+        for node in sortedC:
             if node not in visited:
                 newlist = visited.copy()
                 newlist.append(self)
-                node.scoreNodes(newlist)
+                node.scoreNodes(dest, newlist, cap)
 
 def nodeFromXY(gridWidth, gridHeight, x, y, allNodes):
     index = gridWidth * y + x
@@ -65,7 +90,7 @@ def loadGrid(lines):
         for x, val in enumerate(row): 
             node = Node(x, y,
                 ord(lines[y][x]),
-                start[0] == x and start[1] == y,
+                #start[0] == x and start[1] == y,
                 end[0] == x and end[1] == y)
             nodes.append(node)
 
@@ -82,10 +107,41 @@ def loadLines(filepath):
         lines = f.read().splitlines()
     return lines 
 
-def shortestPath(start, end, nodes):
-    start.scoreNodes()
+def shortestPath(start, end, nodes, cap=BIGSCORE):
+    start.scoreNodes(end, [], cap)
     print('Finding path...')
     return end.smallestScore
+
+def resetScores(nodes):
+    for n in nodes:
+        n.smallestScore = BIGSCORE
+
+def buildGroupsOfLetter(nodes, letter):
+    groups = []
+    letterVal = ord(letter)
+    print('Building groups of letter {}'.format(letter))
+    building = False
+    group = None
+    # only horizontal, ignoring vertical for now to keep code simpler
+    for n in nodes:
+        if n.elevation != letterVal:
+            if building:
+                building = False
+                groups.append(group)
+            continue
+        if not building:
+            building = True
+            group = []
+        group.append(n)
+    return groups
+
+def part2():
+    # Can we take advantage of the fact that many 'a' items are grouped together
+    # Pick from one in the group, should be able to rule out many with this
+    # Additionally, we can rule out a group early if any item in that group
+    # can't reach the destination
+    # we still need to make the single case faster first
+    print('Part 2 Idea')
 
 def main():
     print('AOC day 12')
@@ -94,10 +150,38 @@ def main():
     testgrid = loadGrid(loadLines('input2.txt'))
     grid = loadGrid(loadLines('input.txt'))
     shortest = shortestPath(testgrid[0], testgrid[1], testgrid[2])
+    startTime = time.time()
     shortestPart1 = shortestPath(grid[0], grid[1], grid[2])
+    endTime = time.time()
     print('Shortest Test = {}'.format(shortest))
-    print('Shortest Part 1= {}'.format(shortestPart1))
+    print('Shortest Part 1 = {} Time Taken = {}'.format(shortestPart1, endTime-startTime))
     #31 is correct on the test
-    #457 is too high
+    #456 is correct 
+    print('Part 2. Start from all "a" elevations')
+    starts = list(filter(lambda n : n.elevation == ord('a'), grid[2]))
+    print('Number of a elevations {}'.format(len(grid[2])))
+    #return # skip for now
+    part2Score = BIGSCORE
+    groups = buildGroupsOfLetter(grid[2], 'a')
+    print('Number of horizontal groups {}'.format(len(groups)))
+    groupResults = []
+    smallestSoFar = BIGSCORE
+    resetScores(grid[2])
+    for i, group in enumerate(groups):
+        numIgnored = 0
+        for j in grid[2]:
+            if j.noPath:
+                numIgnored += 1
+        print('Checking group - {} num ignored {}'.format(i, numIgnored))
+        # look at first element of group to build a group score
+        # also record the size of the group
+        #resetScores(grid[2]) # commenting this out is dodgy
+        #incredibly lucky that I didn't need to look in individual groups
+        dist = shortestPath(group[0], grid[1], grid[2], smallestSoFar)
+        if dist < smallestSoFar:
+            smallestSoFar = dist # so for future searches we can early out sooner
+        groupResults.append((dist, len(group), i))
+    print (groupResults)
+    #print('Part 2 answer = {}'.format(part2Score))
 
 main()
