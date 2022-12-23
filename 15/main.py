e63f3650292783df1ac470dcdeec968312501e0b
stephen surtees <stephen@stephens-mbp.home>
2022-12-23 10:13:46 +0000

    Fixed the bug in part 1. We weren't checking if the test square was already a beacon we knew about. THe + 0 hack worked by fluke both in the test and part 1

--- 15/main.py
+++ 15/main.py
@@ -25,51 +25,38 @@ class Board():
     def __init__(self):
         self.sensors = []
         self.beacons = []
-        self.gridHash = {}
+        self.sensorHash = set() 
+        self.beaconHash = set()
 
     def addSensor(self, sensor):
         self.sensors.append(sensor)
-        self.gridHash[sensor.hash()] = True
+        self.sensorHash.add(sensor.hash())
 
     def addBeacon(self, beacon):
         self.beacons.append(beacon)
-        self.gridHash[beacon.hash()] = True
+        self.beaconHash.add(beacon.hash())
 
     def cantBeBeaconCount(self, row):
-        xMin =  100000000
-        xMax = -100000000
-        manhatMax = 0
-
-        # Find the max number of positions in the row we would need to test
-        for s in self.sensors:
-            if s.x < xMin:
-                xMin = s.x
-            if s.x > xMax:
-                xMax = s.x
-            if s.closestDistance > manhatMax:
-                manhatMax = s.closestDistance
-
-        xMin -= manhatMax
-        xMax += manhatMax
-
         # All positions, sorted by x position
-        #positions = list(map(lambda x: Position(x, row), range(xMin, xMax + 1)))
-        notAllowed = {}
+        notAllowed = set() 
+        print('PLEASE', len(notAllowed) )
 
         # See which sensors cover the position
         # use spation partitioning so we don't need to check them all
         for sensor in self.sensors:
             yDiff = abs(sensor.y - row)
             xRadius = sensor.closestDistance - yDiff
-            if xRadius <= 0:
+            if xRadius < 0:
                 continue # This sensor does not overlap with any positions
             xStart = sensor.x - xRadius 
             xEnd = sensor.x + xRadius
             for x in range(xStart, xEnd + 1):
                 pos = Position(x, row) 
                 hash = pos.hash()
+                if hash in self.beaconHash:
+                    continue # If we know this is a beacon, early out
                 if hash not in notAllowed:
-                    notAllowed[hash] = True
+                    notAllowed.add(hash)
 
         print('Beacons')
         return len(notAllowed) 
@@ -84,6 +71,7 @@ def readBoard(filepath):
         coords = re.findall(r'x=(-?\d+), y=(-?\d+)', line)
         beacon = Beacon(int(coords[1][0]), int(coords[1][1]))
         sensor = Sensor(int(coords[0][0]), int(coords[0][1]), beacon)
+        # print ('sensor {} {} beacon {} {}'.format(sensor.x, sensor.y, beacon.x, beacon.y))
         board.addBeacon(beacon)
         board.addSensor(sensor)
 
@@ -91,8 +79,9 @@ def readBoard(filepath):
 
 def part1():
     board = readBoard('input.txt')
+    print('Num beacons' , len(board.beacons))
     cannotBeCount = board.cantBeBeaconCount(2000000) 
-    # 5870798 is too low... really?
+    # Correct ans is 5870800
     print('Part 1 ans', cannotBeCount)
 
 def part2():
